519c5f9a0613dabac9120f5dbd3c660a63be3b8d
Deborah Souza <deborah.souza@rsinet.com.br>
2020-02-06 15:50:28 -0300

    pageobjectmanager

--- src/main/java/br/com/rsinet/hub_bdd/AdvantageMobileBDD/managers/PageObjectManager.java
+++ src/main/java/br/com/rsinet/hub_bdd/AdvantageMobileBDD/managers/PageObjectManager.java
@@ -1,15 +1,17 @@
 package br.com.rsinet.hub_bdd.AdvantageMobileBDD.managers;
 
+import org.openqa.selenium.WebDriver;
+
 public class PageObjectManager {
 	private WebDriver driver;
-	private HomeScreen home;
+//	private HomeScreen home;
 
 	public PageObjectManager(AndroidDriver<AndroidElement> driver) {
 		this.driver = driver;
 	}
 
-	public HomeScreen getHomeScreen() {
-		return (home == null) ? home = new HomeScreen(driver) : home;
-	}
+//	public HomeScreen getHomeScreen() {
+//		return (home == null) ? home = new HomeScreen(driver) : home;
+//	}
 
 }
